e190d5dc420b18f7ee3636eb49ff70f53be1ec8a
yuvysingh <yuvmeister1@gmail.com>
2025-08-13 19:45:48 +0100

    Added ticket page

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -19,7 +19,12 @@ export default async function TicketsPage() {
         console.log(ticketError);
         throw ticketError;
     }
-
-    return <TicketsClient initialTickets={tickets}/>
+    return <main className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <ol>
+                {tickets.map(t => (
+              <li key={t.id}>{t.phase} ticket for sale</li>
+            ))}
+            </ol>
+            </main>
 }
 
